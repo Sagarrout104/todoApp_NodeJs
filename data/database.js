ac46d1d59ef30cc326bb8b0391b1f01eba86bb91
Sagar Rout <sagarrout104@gmail.com>
2024-01-02 13:23:41 +0530

    Fixed engine

--- data/database.js
+++ data/database.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
-const dbConnect = async () => {
-  try {
-    await mongoose.connect(process.env.DB_URL, { dbName: "todo" });
-    console.log("db connection successfull!");
-  } catch (error) {
-    console.log(error);
-  }
+const dbConnect = () => {
+  mongoose
+    .connect(process.env.DB_URL, { dbName: "todo" })
+    .then((c) => {
+      console.log(`db connection successfull with ${c.connection.host}`);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 };
 
 export default dbConnect;
